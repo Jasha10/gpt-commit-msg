44d27feec997a86c6036240af54fd0b662262e20
Jasha <8935917+Jasha10@users.noreply.github.com>
2023-08-01 22:03:08 +0000

    black formatting

--- gpt_commit_msg.py
+++ gpt_commit_msg.py
@@ -11,10 +11,8 @@ import textwrap
 
 import llmlib
 
-max_token_count = {
-    "gpt-4": 8192,
-    "gpt-3.5-turbo": 4097
-}
+max_token_count = {"gpt-4": 8192, "gpt-3.5-turbo": 4097}
+
 
 def log(path: Path | None, text: str) -> None:
     if path:
@@ -45,15 +43,17 @@ def commit_message(llm, diff, prompt, logfile: Path | None = None):
     result.insert(0, llm.ask(prompt + overall_summary))
     return "\n\n".join(result)
 
-def summarize(llm,
-              text,
-              splitre=(
-                r"^(diff )", # First try to split by diff
-                "^$",        # Then try blank line
-                "\n",        # Then try newline
-                ),
-                prompt="Make an unordered list of the effects of every change in this diff.\n\n"
-                ):
+
+def summarize(
+    llm,
+    text,
+    splitre=(
+        r"^(diff )",  # First try to split by diff
+        "^$",  # Then try blank line
+        "\n",  # Then try newline
+    ),
+    prompt="Make an unordered list of the effects of every change in this diff.\n\n",
+):
     query = prompt + text
     tcount = llm.get_num_tokens(query)
 
@@ -82,7 +82,9 @@ def summarize(llm,
             chunk = []
             if llm.get_num_tokens(text) > max_token_count[args.model]:
                 # Need to split using a different regex
-                summaries.extend(summarize(llm, text, splitre=splitre[1:], prompt=prompt))
+                summaries.extend(
+                    summarize(llm, text, splitre=splitre[1:], prompt=prompt)
+                )
             else:
                 summaries.append(llm.ask(prompt + text))
             chunk_tcount = sum(llm.get_num_tokens(c) for c in chunk)
@@ -90,49 +92,83 @@ def summarize(llm,
         chunk_tcount += part_tcount
     return summaries
 
+
 args = None
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="""Use GPT to create source control commit messages.
 
         Unless other arguments are passed, reads the diff from stdin.
-        """)
-    parser.add_argument("--git", "-g", help="Use staged git changes.",
-                        action="store_true")
-    parser.add_argument("--4", "-4", help="Use GPT4 (slower, costs more money)",
-                        dest='gpt4', action="store_true")
+        """
+    )
+    parser.add_argument(
+        "--git", "-g", help="Use staged git changes.", action="store_true"
+    )
+    parser.add_argument(
+        "--4",
+        "-4",
+        help="Use GPT4 (slower, costs more money)",
+        dest="gpt4",
+        action="store_true",
+    )
+
     def parse_temperature(value: str) -> float:
         temp = float(value)
         if 0 <= temp <= 2:
             return temp
-        raise argparse.ArgumentTypeError(f"Temperature must be between 0 and 2, not {value}")
-    parser.add_argument("--temperature", "-t", help=("What sampling temperature to use, between 0 and 2."
-                                                     " Higher values like 0.8 will make the output more random,"
-                                                     " while lower values like 0.2 will make it more focused and deterministic."
-                                                     " Default: 0.0 (to give deterministic and precise output)."),
-                        action="store", metavar="[0-2]", type=parse_temperature,
-                        default=0.0)
-    parser.add_argument("--verbose", "-v", help="Print verbose output",
-                        action="store_true")
-    parser.add_argument("--prompt", "-p", help="Custom prompt to use", action="store",
-                        default=re.sub(r"\s+", " ",
-                            """Write a git commit message for the following. The message
+        raise argparse.ArgumentTypeError(
+            f"Temperature must be between 0 and 2, not {value}"
+        )
+
+    parser.add_argument(
+        "--temperature",
+        "-t",
+        help=(
+            "What sampling temperature to use, between 0 and 2."
+            " Higher values like 0.8 will make the output more random,"
+            " while lower values like 0.2 will make it more focused and deterministic."
+            " Default: 0.0 (to give deterministic and precise output)."
+        ),
+        action="store",
+        metavar="[0-2]",
+        type=parse_temperature,
+        default=0.0,
+    )
+    parser.add_argument(
+        "--verbose", "-v", help="Print verbose output", action="store_true"
+    )
+    parser.add_argument(
+        "--prompt",
+        "-p",
+        help="Custom prompt to use",
+        action="store",
+        default=re.sub(
+            r"\s+",
+            " ",
+            """Write a git commit message for the following. The message
                             starts with a one-line summary of 60 characters, followed by a
                             blank line, followed by a longer but concise description of the
-                            change.""") + "\n\n"
-                       )
-    parser.add_argument("--quiet", "-q", help="Suppress printing of cache hit counter info",
-                        action="store_true")
-    parser.add_argument("--logfile", "-l", help="Log file to use",
-                        action="store", required=False)
-    parser.add_argument("--skip-cache", "-s", help="Skip cache",
-                        action="store_true")
+                            change.""",
+        )
+        + "\n\n",
+    )
+    parser.add_argument(
+        "--quiet",
+        "-q",
+        help="Suppress printing of cache hit counter info",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--logfile", "-l", help="Log file to use", action="store", required=False
+    )
+    parser.add_argument("--skip-cache", "-s", help="Skip cache", action="store_true")
     global args
     args = parser.parse_args()
 
     if args.git:
-        diff = (
-            subprocess.check_output(['git', 'diff', '--cached']).decode('utf-8'))
+        diff = subprocess.check_output(["git", "diff", "--cached"]).decode("utf-8")
     else:
         diff = sys.stdin.read()
     if len(diff) < 5:
@@ -143,19 +179,24 @@ def main():
         args.model = "gpt-4"
     else:
         args.model = "gpt-3.5-turbo"
-    
+
     if args.quiet:
         quiet = True
     else:
         quiet = False
 
     if args.logfile:
-        logging.basicConfig(filename=os.path.expanduser(args.logfile), level=logging.INFO)
+        logging.basicConfig(
+            filename=os.path.expanduser(args.logfile), level=logging.INFO
+        )
 
     logging.info(f"Got args: {args}")
 
-    llm = llmlib.Llm(llmlib.Openai(model=args.model, temperature=args.temperature),
-                     verbose=args.verbose, skip_cache=args.skip_cache)
+    llm = llmlib.Llm(
+        llmlib.Openai(model=args.model, temperature=args.temperature),
+        verbose=args.verbose,
+        skip_cache=args.skip_cache,
+    )
 
     message = commit_message(llm, diff, args.prompt)
     logging.info(f"GPT returned:\n{message}")
@@ -167,6 +208,7 @@ def main():
         print(f"({llm.counter_string()})")
 
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
